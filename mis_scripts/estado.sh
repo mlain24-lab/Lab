60faf7b46ec8b93b8d3902f773d3d4613c39320a
mlain24 <mlain24@gmail.com>
2026-03-19 20:43:17 +0000

    primeros scripts

---
+++ mis_scripts/estado.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "====================================="
+echo "🤖 Bienvenido a tu Servidor, Administrador."
+echo "====================================="
+echo ""
+echo "📅 La fecha y hora actual del sistema es:"
+date
+echo ""
+echo "💾 El estado de la memoria RAM es:"
+free -m
+echo ""
+echo "✅ Chequeo completado."
